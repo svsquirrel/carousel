19c87e6554c2db92d8369bbd1fc9fa59894f2f4d
svsquirrel <suekittle@hotmail.com>
2021-09-23 14:37:52 -0500

    have dots working but now if go back up to arrows they don't work properly

--- index.js
+++ index.js
@@ -1,17 +1,71 @@
-document.addEventListener('DOMContentLoaded', function () {
-    renderContent()
-});
-
+// document.addEventListener('DOMContentLoaded', function () {
+//     renderContent()
+// });
+window.onload = renderContent;
 const ImageData = [
-    { imgsrc: "images/rainbow.jpg", alttext: "seaside rainbow" },
-    { imgsrc: "images/ocean.jpg", alttext: "rough ocean" },
-    { imgsrc: "images/greysky.jpg", alttext: "big rock in Alaska" },
-    { imgsrc: "images/rock.jpg", alttext: "seagull by sea" },
-    { imgsrc: "images/sharks.jpg", alttext: "huge aquarium with sharks" },
-    { imgsrc: "images/monarch.jpg", alttext: "monarch on a flower" },
-    { imgsrc: "images/river.jpg", alttext: "sunshine on the Yellow River" }
+    { imgsrc: "images/rainbow.jpg", alttext: "seaside rainbow",               caption: 'Rainbow over Fort Lauderdale' },
+    { imgsrc: "images/ocean.jpg",   alttext: "rough ocean" ,                  caption: 'Sailing the Gulf Stream'},
+    { imgsrc: "images/greysky.jpg", alttext: "big rock in Alaska",            caption: 'I forget this rocks name'},
+    { imgsrc: "images/rock.jpg",    alttext: "seagull by sea",                caption: 'A seagull riding out the storm Navarre, Florida' },
+    { imgsrc: "images/sharks.jpg",  alttext: "huge aquarium with sharks",     caption: 'A third grade class visits Tampa Aquarium' },
+    { imgsrc: "images/monarch.jpg", alttext: "monarch on a flower",           caption: 'A monarch tastes bougainvilla' },
+    { imgsrc: "images/river.jpg",   alttext: "sunshine on the Yellow River" , caption: 'An inlet beckons on the Yellow River'}
 ]
 
+function renderContent() {
+    const content = document.querySelector('.content');
+    const ulist = document.querySelector('.indicators');
+
+    for (let i = 0; i < ImageData.length; i++) {
+
+        const imageDisplay = document.createElement('img');
+        const imgsource = ImageData[i].imgsrc;
+        const alttext = ImageData[i].alttext;
+        imageDisplay.src = imgsource;
+        imageDisplay.setAttribute('alt', alttext);
+        imageDisplay.classList.add('image');
+        imageDisplay.dataset.id = i;
+        if (imageDisplay.dataset.id == '0') {
+            imageDisplay.classList.add('active');
+        }
+
+        const indicatorItem = document.createElement('li');
+        indicatorItem.dataset.id = i;
+        indicatorItem.addEventListener('click', () =>{
+                showImage(i);
+        })
+        if (indicatorItem.dataset.id == '0'){
+            indicatorItem.classList.add('active');
+        }
+        content.appendChild(imageDisplay);
+        ulist.appendChild(indicatorItem);
+    }
+    
+}
+const indicators = document.querySelectorAll('li');
+      indicators.forEach(li => {
+          li.addEventListener('click', () => {
+              showImage(dataset.id)
+          })
+      })
+
+function showImage(id){
+    console.log(id);
+   
+    const items = document.querySelectorAll('img');
+        items.forEach(item  =>{
+            item.classList.remove('active')
+        })
+    const indicators = document.querySelectorAll('li');
+        indicators.forEach(li => {
+            li.classList.remove('active');
+    })
+
+
+    items[id].classList.add('active');
+    indicators[id].classList.add('active');
+
+}
 let count = 0;
 let indicatorscount = 0;
 const nextItem = document.querySelector('.right');
@@ -79,37 +133,4 @@ function keyPress(e) {
       showNextItem();
     }
   }
-
-
-function renderContent() {
-    const content = document.querySelector('.content');
-    const ulist = document.querySelector('.indicators');
-
-    for (let i = 0; i < ImageData.length; i++) {
-
-        const imageDisplay = document.createElement('img');
-        const imgsource = ImageData[i].imgsrc;
-        const alttext = ImageData[i].alttext;
-        imageDisplay.src = imgsource;
-        imageDisplay.setAttribute('alt', alttext);
-        imageDisplay.classList.add('image');
-        imageDisplay.dataset.id = i;
-        if (imageDisplay.dataset.id == '0') {
-            imageDisplay.classList.add('active');
-        }
-
-        const indicatorItem = document.createElement('li');
-        indicatorItem.id = 'indicator-' + [i];
-
-        content.appendChild(imageDisplay);
-        ulist.appendChild(indicatorItem);
-    }
-    const activeli = document.querySelector('#indicator-0')
-    activeli.classList.add('active');
-
-}
-
-
-
-
 
